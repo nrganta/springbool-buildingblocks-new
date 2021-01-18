0f2b6391187ad3c4527ac82e69e6db110e0aad5f
nrgan <nrganta@yahoo.com>
2021-01-18 16:34:52 -0600

    Second commit

--- src/main/java/com/stacksimply/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimply/restservices/SpringbootBuildingblocksApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
+//this is commenting
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
