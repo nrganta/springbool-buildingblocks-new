056ecd2b98ae644aafd4156e78270cfd0e2b8133
nrgan <nrganta@yahoo.com>
2021-01-24 22:20:20 -0600

    Response status Exception First code commit

---
+++ src/main/java/com/stacksimply/restservices/exceptions/UserExistsException.java
@@ -0,0 +1,18 @@
+package com.stacksimply.restservices.exceptions;
+
+
+public class UserExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1904585489531578456L;
+
+	public UserExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
---
+++ src/main/java/com/stacksimply/restservices/exceptions/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.stacksimply.restservices.exceptions;
+
+public class UserNotFoundException  extends Exception {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+		}
+	
+	
+
+}
